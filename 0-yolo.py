d5bd1d710d34f75e1e0f205201d73348acf6f16f
xica369 <761@holbertonschool.com>
2020-08-13 02:43:04 +0000

    0-yolo fixed pep8

--- 0-yolo.py
+++ 0-yolo.py
@@ -15,13 +15,14 @@ class Yolo:
           for the Darknet model, listed in order of index, can be found
         - class_t is a float representing the box score threshold for
           the initial filtering step
-        - nms_t is a float representing the IOU threshold for non-max suppression
+        - nms_t is a float representing the IOU threshold for non-max
+          suppression
         - anchors is a numpy.ndarray of shape (outputs, anchor_boxes, 2)
           containing all of the anchor boxes:
-           * outputs is the number of outputs (predictions)
-             made by the Darknet model
-           * anchor_boxes is the number of anchor boxes used for each prediction
-           * 2 => [anchor_box_width, anchor_box_height]"""
+          * outputs is the number of outputs (predictions)
+            made by the Darknet model
+          * anchor_boxes is the number of anchor boxes used for each prediction
+          * 2 => [anchor_box_width, anchor_box_height]"""
 
         # Public instance attributes
         self.model = K.models.load_model(model_path)
