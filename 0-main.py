9b9b5d3e36a028314e3a34659c71f54b34671423
xica369 <761@holbertonschool.com>
2020-08-13 02:22:33 +0000

    0-main pep8

--- 0-main.py
+++ 0-main.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     anchors = np.array([[[116, 90], [156, 198], [373, 326]],
                         [[30, 61], [62, 45], [59, 119]],
                         [[10, 13], [16, 30], [33, 23]]])
-    yolo = Yolo('../data/yolo.h5', '../data/coco_classes.txt', 0.6, 0.5, anchors)
+    yolo = (Yolo('../data/yolo.h5',
+                 '../data/coco_classes.txt', 0.6, 0.5, anchors))
     yolo.model.summary()
     print('Class names:', yolo.class_names)
     print('Class threshold:', yolo.class_t)
