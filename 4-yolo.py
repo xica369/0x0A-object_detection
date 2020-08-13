60018ada2859800812c09cfd5386331a765ec8a5
xica369 <761@holbertonschool.com>
2020-08-13 03:14:14 +0000

    update comments

--- 4-yolo.py
+++ 4-yolo.py
@@ -13,22 +13,25 @@ class Yolo:
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """
-        model_path is the path to where a Darknet Keras model is stored
-        classes_path is the path to where the list of class names used
-        for the Darknet model, listed in order of index, can be found
-        class_t is a float representing the box score threshold for
-        the initial filtering step
-        nms_t is a float representing the IOU threshold for non-max suppression
-        anchors is a numpy.ndarray of shape (outputs, anchor_boxes, 2)
-        containing all of the anchor boxes:
-          outputs is the number of outputs (predictions)
-          made by the Darknet model
-          anchor_boxes is the number of anchor boxes used for each prediction
-          2 => [anchor_box_width, anchor_box_height]"""
+        - model_path is the path to where a Darknet Keras model is stored
+        - classes_path is the path to where the list of class names used
+          for the Darknet model, listed in order of index, can be found
+        - class_t is a float representing the box score threshold for
+          the initial filtering step
+        - nms_t is a float representing the IOU threshold for non-max
+          suppression
+        - anchors is a numpy.ndarray of shape (outputs, anchor_boxes, 2)
+          containing all of the anchor boxes:
+          * outputs is the number of outputs (predictions)
+            made by the Darknet model
+          * anchor_boxes is the number of anchor boxes used for each prediction
+          * 2 => [anchor_box_width, anchor_box_height]"""
 
+        # Public instance attributes
         self.model = K.models.load_model(model_path)
 
         self.class_names = []
+
         with open(classes_path, "r") as file:
             for line in file:
                 class_name = line.strip()
@@ -39,7 +42,34 @@ class Yolo:
         self.anchors = anchors
 
     def process_outputs(self, outputs, image_size):
-        """Process Outputs"""
+        """
+        public method
+        - outputs is a list of numpy.ndarrays containing the predictions from
+          the Darknet model for a single image:
+          Each output will have the shape:
+          (grid_height, grid_width, anchor_boxes, 4 + 1 + classes)
+            * grid_height & grid_width => the height and width of the grid used
+              for the output
+            * anchor_boxes => the number of anchor boxes used
+            * 4 => (t_x, t_y, t_w, t_h)
+            * 1 => box_confidence
+            * classes => class probabilities for all classes
+        - image_size is a numpy.ndarray containing the image’s original size
+          [image_height, image_width]
+        Returns a tuple of (boxes, box_confidences, box_class_probs):
+        - boxes: a list of numpy.ndarrays of shape
+          (grid_height, grid_width, anchor_boxes, 4) containing the processed
+          boundary boxes for each output, respectively:
+          * 4 => (x1, y1, x2, y2)
+          * (x1, y1, x2, y2) should represent the boundary box relative to
+            original image
+        - box_confidences: a list of numpy.ndarrays of shape
+          (grid_height, grid_width, anchor_boxes, 1) containing the box
+          confidences for each output, respectively
+        - box_class_probs: a list of numpy.ndarrays of shape
+          (grid_height, grid_width, anchor_boxes, classes) containing the box’s
+          class probabilities for each output, respectively
+        """
 
         boxes = []
         box_confidences = []
@@ -126,7 +156,25 @@ class Yolo:
         return (boxes, box_confidences, box_class_probs)
 
     def filter_boxes(self, boxes, box_confidences, box_class_probs):
-        """Filter Boxes"""
+        """
+        public method to Filter Boxes
+        - boxes: a list of numpy.ndarrays of shape
+          (grid_height, grid_width, anchor_boxes, 4) containing the processed
+          boundary boxes for each output, respectively
+        - box_confidences: a list of numpy.ndarrays of shape
+          (grid_height, grid_width, anchor_boxes, 1) containing the processed
+          box confidences for each output, respectively
+        - box_class_probs: a list of numpy.ndarrays of shape
+          (grid_height, grid_width, anchor_boxes, classes) containing the
+          processed box class probabilities for each output, respectively
+        Returns a tuple of (filtered_boxes, box_classes, box_scores):
+        - filtered_boxes: a numpy.ndarray of shape (?, 4) containing all of
+          the filtered bounding boxes:
+        - box_classes: a numpy.ndarray of shape (?,) containing the class
+          number that each box in filtered_boxes predicts, respectively
+        - box_scores: a numpy.ndarray of shape (?) containing the box scores
+          for each box in filtered_boxes, respectively
+        """
 
         class_t = self.class_t
         filtered_boxes = []
@@ -166,8 +214,25 @@ class Yolo:
 
     def non_max_suppression(self, filtered_boxes, box_classes, box_scores):
         """
-        method that applies Non-max Suppression
+        public method that applies Non-max Suppression
+        - filtered_boxes: a numpy.ndarray of shape (?, 4) containing all of
+          the filtered bounding boxes:
+        - box_classes: a numpy.ndarray of shape (?,) containing the class
+          number for the class that filtered_boxes predicts, respectively
+        - box_scores: a numpy.ndarray of shape (?) containing the box scores
+          for each box in filtered_boxes, respectively
+        Returns a tuple of
+        (box_predictions, predicted_box_classes, predicted_box_scores):
+        - box_predictions: a numpy.ndarray of shape (?, 4) containing all of
+          the predicted bounding boxes ordered by class and box score
+        - predicted_box_classes: a numpy.ndarray of shape (?,) containing the
+          class number for box_predictions ordered by class and box score,
+          respectively
+        - predicted_box_scores: a numpy.ndarray of shape (?) containing the
+          box scores for box_predictions ordered by class and box score,
+          respectively
         """
+
         box_predictions = []
         predicted_box_classes = []
         predicted_box_scores = []
@@ -221,13 +286,16 @@ class Yolo:
 
     @staticmethod
     def load_images(folder_path):
-        """function that load images
+        """
+        function that load images
 
-        folder_path: a string representing the path to the folder holding all
-        the images to load
+        - folder_path: a string representing the path to the folder holding all
+          the images to load
+
         Returns a tuple of (images, image_paths):
-        images: a list of images as numpy.ndarrays
-        image_paths: a list of paths to the individual images in images"""
+        - images: a list of images as numpy.ndarrays
+        - image_paths: a list of paths to the individual images in images
+        """
 
         images = []
 
